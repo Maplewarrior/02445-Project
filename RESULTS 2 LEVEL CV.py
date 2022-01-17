cb1fed8f322aa9e38ea58ee2ff2bfae31c90641a
Maplewarrior <michaelharborg@gmail.com>
2022-01-17 19:13:13 +0100

    Create RESULTS 2 LEVEL CV.py

---
+++ RESULTS 2 LEVEL CV.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+####### For (y,z) coordinates
+E_gen_RF_final_yz = np.array([[0.73125],
+       [0.8    ],
+       [0.7375 ],
+       [0.80625],
+       [0.78125],
+       [0.76875],
+       [0.76875],
+       [0.75625],
+       [0.76875],
+       [0.76875],    ])
+
+
+E_gen_ANN_final_yz = np.array([[0.83125],
+       [0.81875],
+       [0.75625],
+       [0.88125],
+       [0.85   ],
+       [0.8    ],
+       [0.81875],
+       [0.85   ],
+       [0.8375 ],
+       [0.875  ],   ])
+
+E_gen_LogReg_final_yz = np.array([[0.7375 ],
+       [0.74375],
+       [0.75   ],
+       [0.8375 ],
+       [0.79375],
+       [0.75   ],
+       [0.76875],
+       [0.79375],
+       [0.7375 ],
+       [0.76875],   ])
+
+
+E_gen_RF_final_xz = np.array([])
+E_gen_ANN_final_xz = np.array([])
+E_gen_LogReg_final_xz = np.array([])
+
+
+E_gen_RF_final_xyz = np.array([])
+E_gen_ANN_final_xyz = np.array([])
+E_gen_LogReg_final_xyz = np.array([])
+
+E_gen_RF_final_xy = np.array([])
+E_gen_ANN_final_xy = np.array([])
+E_gen_LogReg_final_xy = np.array([])
